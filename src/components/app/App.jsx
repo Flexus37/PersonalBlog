6e0c9fc9df9f1899e7f785fd2958f7b707e7d0c2
Flexus37 <aleshadudin53@gmail.com>
2023-11-26 00:25:13 +0300

    SignUp and LogIn auth functional

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,19 +1,36 @@
+import { useEffect } from "react";
 import { Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useSelector } from 'react-redux'
 
 import Header from "../header/Header";
 import Sidebar from "../sidebar/Sidebar";
 import { MainPage, ProfilePage, WorksPage, SearchResultsPage } from "../pages";
+import { LogIn, SignUp } from "../auth";
+// import { selectAll } from "../../services/api/userInfoSlice";
 
 import SinglePost from "../posts/SinglePost";
-
-import LogIn from "../auth/LogIn";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
 import './app.scss';
 
-import { useEffect } from "react";
+
 
 function App() {
+  const {isAuthenticated} = useSelector(state => state.userInfo);
+
+  if (!isAuthenticated) {
+    return (
+      <Router>
+        <Suspense fallback={<ErrorMessage />}>
+          <Routes>
+            <Route path="/" element={<LogIn />} />
+            <Route path='/registration' element={<SignUp />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    )
+  }
 
   return (
     <Router>
@@ -22,8 +39,7 @@ function App() {
 
         <main className="main">
             <div className="container">
-                {/* <Suspense fallback> */}
-                {/* <LogIn /> */}
+                <Suspense fallback={<ErrorMessage />}>
                   <Sidebar/>
                   <Routes>
                     <Route path="/" element={<MainPage/>} />
@@ -33,7 +49,7 @@ function App() {
                     <Route path='/search-results' element={<SearchResultsPage/>} />
                   </Routes>
 
-                {/* </Suspense> */}
+                </Suspense>
             </div>
         </main>
       </div>
--- src/services/store/index.js
+++ src/services/store/index.js
@@ -1,18 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../api/apiSlice";
-
-// const timeMiddleware = () => (next) => (action) => {
-//     const res = new Date(action);
-//     if (res.getDate > 0) {
-//         return next({
-//             type: res
-//         })
-//     }
-//     return next(action);
-// }
+import userInfo from '../api/userInfoSlice';
 
 const store = configureStore({
-    reducer: {[apiSlice.reducerPath]: apiSlice.reducer},
+    reducer: {userInfo, [apiSlice.reducerPath]: apiSlice.reducer},
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: process.env.NODE_ENV !== 'production'
