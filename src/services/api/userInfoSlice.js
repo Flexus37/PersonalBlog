c6afef1bd963e8d40ae0c23faa2d1752f0f81403
Flexus37 <aleshadudin53@gmail.com>
2023-11-25 22:05:52 +0300

    Created userInfoSlice

---
+++ src/services/api/userInfoSlice.js
@@ -0,0 +1,31 @@
+import { createEntityAdapter, createSlice } from "@reduxjs/toolkit"
+
+const userInfoAdapter = createEntityAdapter();
+const initialState = userInfoAdapter.getInitialState({
+    id: null,
+    isAuthenticated: false,
+    loadingStatus: 'idle'
+});
+
+const userInfoSlice = createSlice({
+    name: 'userInfo',
+    initialState,
+    reducers: {
+        setUserAuthentication: (state, action) => {
+            state.isAuthenticated = action.payload;
+        },
+        setUserId: (state, action) => {
+            state.id = action.payload;
+        }
+    }
+})
+
+const {actions, reducer} = userInfoSlice;
+
+export const {selectAll} = userInfoAdapter.getSelectors(state => state.userInfo);
+
+export default reducer;
+export const {
+    setUserAuthentication,
+    setUserId
+} = actions;
