82e9ed43274a81d048562acd971a6f5f15fe4074
Flexus37 <aleshadudin53@gmail.com>
2024-03-20 22:33:35 +0300

    Create ErrorBoundary

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,21 +1,22 @@
-import { useEffect } from "react";
-import { Suspense } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import { auth } from "../../services/firebase/FirestoreConfig";
-import { onAuthStateChanged } from "firebase/auth";
-import { useSelector, useDispatch } from 'react-redux'
-import { setUserAuthentication, setUserId } from "../../services/api/userInfoSlice";
+import { onAuthStateChanged } from "firebase/auth"
+import { Suspense, useEffect } from "react"
+import { useDispatch, useSelector } from 'react-redux'
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import { setUserAuthentication, setUserId } from "../../services/api/userInfoSlice"
+import { auth } from "../../services/firebase/FirestoreConfig"
 
-import Header from "../header/Header";
-import Sidebar from "../sidebar/Sidebar";
-import { MainPage, ProfilePage, WorksPage, SearchResultsPage, FriendsPage, FriendRequestsPage } from "../pages";
-import { SignIn, SignUp } from "../auth";
+import { SignIn, SignUp } from "../auth"
+import Header from "../header/Header"
+import { FriendRequestsPage, FriendsPage, MainPage, ProfilePage, SearchResultsPage, WorksPage } from "../pages"
+import Sidebar from "../sidebar/Sidebar"
 // import { selectAll } from "../../services/api/userInfoSlice";
 
-import SinglePost from "../posts/SinglePost";
-import ErrorMessage from "../errorMessage/ErrorMessage";
+// import SinglePost from "../posts/SinglePost";
+import ErrorBoundary from '../errorBoundary/ErrorBoundary'
+import ErrorMessage from "../errorMessage/ErrorMessage"
+import Spinner from '../spinner/Spinner'
 
-import './app.scss';
+import './app.scss'
 
 
 
@@ -44,32 +45,36 @@ function App() {
 
             <main className="main">
                 <div className="container">
-                    <Suspense fallback={<ErrorMessage />}>
-                      <Sidebar />
-                      <Routes>
-                        <Route path="/" element={<Navigate replace to={`/blog/${userId}`} />} />
-                        <Route path="/sign-in" element={<Navigate replace to={`/blog/${userId}`} />} />
-                        <Route path="/blog/:id" element={<MainPage/>} />
-                        <Route path="/profile" element={<ProfilePage/>} />
-                        <Route path="/works/:id" element={<WorksPage/>} />
-                        <Route path="/friends" element={<FriendsPage />} />
-                        <Route path="/friends/requests" element={<FriendRequestsPage />} />
-                        {/* <Route path="/single-post/:id" element={<SinglePost/>} /> */}
-                        <Route path='/search-results' element={<SearchResultsPage/>} />
-                      </Routes>
+                    <ErrorBoundary>
+                      <Suspense fallback={<Spinner />}>
+                        <Sidebar />
+                        <Routes>
+                          <Route path="/" element={<Navigate replace to={`/blog/${userId}`} />} />
+                          <Route path="/sign-in" element={<Navigate replace to={`/blog/${userId}`} />} />
+                          <Route path="/blog/:id" element={<MainPage/>} />
+                          <Route path="/profile" element={<ProfilePage/>} />
+                          <Route path="/works/:id" element={<WorksPage/>} />
+                          <Route path="/friends" element={<FriendsPage />} />
+                          <Route path="/friends/requests" element={<FriendRequestsPage />} />
+                          {/* <Route path="/single-post/:id" element={<SinglePost/>} /> */}
+                          <Route path='/search-results' element={<SearchResultsPage/>} />
+                        </Routes>
 
-                    </Suspense>
+                      </Suspense>
+                    </ErrorBoundary>
                 </div>
             </main>
           </div>
         ) : (
-          <Suspense fallback={<ErrorMessage />}>
-            <Routes>
-              <Route path="/*" element={<Navigate replace to='/sign-in' />} />
-              <Route path="/sign-in" element={<SignIn />} />
-              <Route path='/registration' element={<SignUp />} />
-            </Routes>
-          </Suspense>
+          <ErrorBoundary>
+            <Suspense fallback={<ErrorMessage />}>
+              <Routes>
+                <Route path="/*" element={<Navigate replace to='/sign-in' />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path='/registration' element={<SignUp />} />
+              </Routes>
+            </Suspense>
+          </ErrorBoundary>
         )
       }
     </Router>
